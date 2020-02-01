3b4d252684e1c9ec20e2be74a02a0a3855223170
Brian Malinconico <bmalinconico@terminus.com>
2020-01-31 19:11:05 -0500

    Adding each_page_method

--- lib/dynamoid_advanced_where/query_materializer.rb
+++ lib/dynamoid_advanced_where/query_materializer.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 require_relative './filter_builder'
 
 module DynamoidAdvancedWhere
   class QueryMaterializer
     include Enumerable
     attr_accessor :query_builder
-
 
     delegate :klass, to: :query_builder
     delegate :table_name, to: :klass
@@ -23,49 +24,72 @@ module DynamoidAdvancedWhere
     def each(&blk)
       return enum_for(:each) unless blk
 
+      each_page.flat_map{|i, _| i }.each(&blk)
+    end
+
+    def each_page
       if must_scan?
-        each_via_scan(&blk)
+        each_page_via_scan
       else
-        each_via_query(&blk)
+        each_page_via_query
       end
     end
 
-    def each_via_query
+    def each_page_via_query
       query = {
-        table_name: table_name,
+        table_name: table_name
       }.merge(filter_builder.to_query_filter)
 
-      results = client.query(query)
+      Enumerator.new do |yielder|
+        start_key = nil
+        loop do
+          results = client.query(query.merge(exclusive_start_key: start_key))
 
-      if results.items
-        results.items.each do |item|
-          yield klass.from_database(item.symbolize_keys)
+          start_key = results.last_evaluated_key
+
+          items = (results.items || []).map do |i|
+            klass.from_database(i.symbolize_keys)
+          end
+
+          yielder.yield(items, results)
+
+          break if start_key.nil?
         end
-      end
+      end.lazy
     end
 
-    def each_via_scan
+    def each_page_via_scan
       query = {
         table_name: table_name
       }.merge(filter_builder.to_scan_filter)
 
-      results = client.scan(query)
+      start_key = nil
+      Enumerator.new do |yielder|
+        loop do
+          results = client.scan(query.merge(exclusive_start_key: start_key))
 
-      if results.items
-        results.items.each do |item|
-          yield klass.from_database(item.symbolize_keys)
+          start_key = results.last_evaluated_key
+
+          items = (results.items || []).map do |i|
+            klass.from_database(i.symbolize_keys)
+          end
+
+          yielder.yield(items, results)
+
+          break if start_key.nil?
         end
-      end
+      end.lazy
     end
 
     def filter_builder
       @filter_builder ||= FilterBuilder.new(
-        query_builder: self.query_builder,
+        query_builder: query_builder,
         klass: klass,
       )
     end
 
     private
+
     def client
       Dynamoid.adapter.client
     end
