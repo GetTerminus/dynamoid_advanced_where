187ff5a39d07b6bad0f618aede82b48f1fb030cc
Brian Malinconico <bmalinconico@terminus.com>
2020-02-04 16:28:33 -0500

    Removing some comments

--- lib/dynamoid_advanced_where/nodes/base_node.rb
+++ lib/dynamoid_advanced_where/nodes/base_node.rb
@@ -4,30 +4,10 @@ module DynamoidAdvancedWhere
   module Nodes
     class BaseNode
       attr_accessor :child_nodes, :expression_prefix
-     # # Method Nodes
-     # def and(other_arg)
-     #   create_subnode(AndNode).tap do |and_node|
-     #     and_node.child_nodes = [self, other_arg].compact
-     #   end
-     # end
-     # alias & and
-
-     # def or(other_arg)
-     #   create_subnode(OrNode).tap do |and_node|
-     #     and_node.child_nodes = [self, other_arg].compact
-     #   end
-     # end
-     # alias | or
-
-     # def negate
-     #   self.and(nil).tap{|n| n.negate! }
-     # end
-     # alias ! negate
 
       def all_nodes
         [self] + Array.wrap(self.child_nodes).flat_map(&:all_nodes)
       end
-
 
       def expression_attribute_names
         {}
@@ -36,23 +16,6 @@ module DynamoidAdvancedWhere
       def expression_attribute_values
         {}
       end
-
-      # def to_condition_expression
-
-      # end
-
-      # def flatten_tree!
-      #   Array.wrap(self.child_nodes).map(&:flatten_tree!)
-      #   self.flatten_logic!
-      # end
-
-      # def flatten_logic!
-
-      # end
-
-      # def expression_prefix
-      #   @expression_prefix ||= SecureRandom.hex
-      # end
     end
   end
 end
