eaafb97e3f29eda178ae1bfae299137ce527d5ea
hfsearcy <houston.searcy@terminus.com>
2020-02-27 09:32:56 -0500

    remove limit because underlying Dynamo behavior is bad

--- lib/dynamoid_advanced_where/query_materializer.rb
+++ lib/dynamoid_advanced_where/query_materializer.rb
@@ -3,7 +3,7 @@ require_relative './filter_builder'
 module DynamoidAdvancedWhere
   class QueryMaterializer
     include Enumerable
-    attr_accessor :query_builder, :limit, :start_key, :last_evaluated_key
+    attr_accessor :query_builder, :start_key
 
     delegate :klass, to: :query_builder
     delegate :table_name, to: :klass
@@ -13,24 +13,11 @@ module DynamoidAdvancedWhere
 
     def initialize(query_builder:)
       self.query_builder = query_builder
-      self.limit = {}
       self.start_key = {}
     end
 
     def all
       each.to_a
-    end
-
-    def next_page
-      @start_key = { exclusive_start_key: nil } if @start_key.empty?
-      result = ResultPage.new(each.to_a)
-      result.last_evaluated_key = @last_evaluated_key
-      result
-    end
-
-    def record_limit(num)
-      @limit = { limit: num }
-      self
     end
 
     def start(key_hash)
@@ -51,12 +38,11 @@ module DynamoidAdvancedWhere
     def each_via_query
       query = {
         table_name: table_name,
-      }.merge(filter_builder.to_query_filter).merge(limit).merge(start_key)
+      }.merge(filter_builder.to_query_filter).merge(start_key)
 
       results = client.query(query)
 
       if results.items
-        @last_evaluated_key = results.last_evaluated_key
         results.items.each do |item|
           yield klass.from_database(item.symbolize_keys)
         end
@@ -66,12 +52,11 @@ module DynamoidAdvancedWhere
     def each_via_scan
       query = {
         table_name: table_name
-      }.merge(filter_builder.to_scan_filter).merge(limit).merge(start_key)
+      }.merge(filter_builder.to_scan_filter).merge(start_key)
 
       results = client.scan(query)
 
       if results.items
-        @last_evaluated_key = results.last_evaluated_key
         results.items.each do |item|
           yield klass.from_database(item.symbolize_keys)
         end
--- spec/dynamoid_advanced_where/search_tests/start_key_spec.rb
+++ spec/dynamoid_advanced_where/search_tests/start_key_spec.rb
@@ -20,22 +20,10 @@ RSpec.describe "Paginated results" do
       ).to match_array [item1, item3]
     end
 
-    it "limits" do
-      result = klass.where{ (simple_string == 'baz') }.record_limit(1).next_page
-      expect(result.last_evaluated_key).to eq({'bar' => '1'})
-      expect(result).to eq([item1])
-    end
-
-    it "limits from a start point" do
+    it "returns from a start point" do
       expect(
-        klass.where{ (simple_string == 'baz') }.start({bar: item1.bar }).record_limit(1).next_page
+        klass.where{ (simple_string == 'baz') }.start({bar: item1.bar }).all
       ).to match_array [item3]
-    end
-
-    it "returns nil on last page" do
-      result = klass.where{ (simple_string == 'baz') }.start({bar: item1.bar }).record_limit(5).next_page
-      expect(result).to match_array [item3]
-      expect(result.last_evaluated_key).to be_nil
     end
   end
 
@@ -47,7 +35,7 @@ RSpec.describe "Paginated results" do
       end
     end
 
-    let!(:item2) { klass.create(simple_string: 'foo', bar: '0', second_string: 'a') }
+    let!(:item0) { 4.times { |i| klass.create(simple_string: 'foo', bar: "junk#{i}", second_string: 'a') } }
     let!(:item1) { klass.create(simple_string: 'baz', bar: '1', second_string: 'x') }
     let!(:item2) { klass.create(simple_string: 'foo', bar: '2', second_string: 'y') }
     let!(:item3) { klass.create(simple_string: 'baz', bar: '3', second_string: 'z') }
@@ -58,16 +46,10 @@ RSpec.describe "Paginated results" do
       ).to match_array [item1, item3]
     end
 
-    it "limits" do
-      result = klass.where{ (simple_string == 'baz') }.start(nil).record_limit(1).next_page
-      expect(result.last_evaluated_key).to eq({'bar' => item1.bar})
-      expect(result).to eq([item1])
-    end
-
     it "limits with start" do
-      start_key = klass.where{ (simple_string == 'baz') }.record_limit(1).next_page.last_evaluated_key
+      start_key = { bar: item1.bar }
       expect(
-        klass.where{ (simple_string == 'baz') }.start(start_key).record_limit(1)
+        klass.where{ (simple_string == 'baz') }.start(start_key).all
       ).to match_array [item3]
     end
   end
